8f80c44a222aa03809b88c02ed38544999fe4aff
knru <rusakov.msk@ya.ru>
2021-07-13 17:54:40 +0300

    first

--- index.py
+++ index.py
@@ -1,59 +1,15 @@
-from scapy.all import *
-from threading import Thread
-import pandas
+import pywifi
 import time
-import os
 
-# initialize the networks dataframe that will contain all access points nearby
-networks = pandas.DataFrame(columns=["BSSID", "SSID", "dBm_Signal", "Channel", "Crypto"])
-# set the index BSSID (MAC address of the AP)
-networks.set_index("BSSID", inplace=True)
+wifi = pywifi.PyWiFi()
+iface = wifi.interfaces()[0]
+iface.scan()
+time.sleep(0.5)
+results = iface.scan_results()
 
-def callback(packet):
-    if packet.haslayer(Dot11Beacon):
-        # extract the MAC address of the network
-        bssid = packet[Dot11].addr2
-        # get the name of it
-        ssid = packet[Dot11Elt].info.decode()
-        try:
-            dbm_signal = packet.dBm_AntSignal
-        except:
-            dbm_signal = "N/A"
-        # extract network stats
-        stats = packet[Dot11Beacon].network_stats()
-        # get the channel of the AP
-        channel = stats.get("channel")
-        # get the crypto
-        crypto = stats.get("crypto")
-        networks.loc[bssid] = (ssid, dbm_signal, channel, crypto)
+# use for or other loop to retrieve you data
 
-
-def print_all():
-    while True:
-        os.system("clear")
-        print(networks)
-        time.sleep(0.5)
-
-
-def change_channel():
-    ch = 1
-    while True:
-        os.system(f"iwconfig {interface} channel {ch}")
-        # switch channel from 1 to 14 each 0.5s
-        ch = ch % 14 + 1
-        time.sleep(0.5)
-
-
-if __name__ == "__main__":
-    # interface name, check using iwconfig
-    interface = "wlan0mon"
-    # start the thread that prints all the networks
-    printer = Thread(target=print_all)
-    printer.daemon = True
-    printer.start()
-    # start the channel changer
-    channel_changer = Thread(target=change_channel)
-    channel_changer.daemon = True
-    channel_changer.start()
-    # start sniffing
-    sniff(prn=callback, iface=interface)
+for i in enumerate(results):    
+    bssid = i.bssid
+    ssid  = i.ssid
+    print("Mac Address" + bssid +"\t" + "access point name" + ssid)
