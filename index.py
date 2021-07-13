85bb3357a5630696b9165dfeef6dcce86c6c2376
knru <rusakov.msk@ya.ru>
2021-07-13 17:56:37 +0300

    first

--- index.py
+++ index.py
@@ -8,8 +8,9 @@ time.sleep(0.5)
 results = iface.scan_results()
 
 # use for or other loop to retrieve you data
-
+print('start scan...')
 for i in enumerate(results):    
     bssid = i.bssid
     ssid  = i.ssid
     print("Mac Address" + bssid +"\t" + "access point name" + ssid)
+print('all done')
