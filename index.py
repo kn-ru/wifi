784904953c64f197ace376abee0eb84e54485368
knru <rusakov.msk@yandex.ru>
2021-07-18 20:06:49 +0300

    add button logic

--- index.py
+++ index.py
@@ -18,19 +18,20 @@ while True:
 
         for wifi_point in wifi_list:
             print('WiFi - ', wifi_point)
-            if wifi_point in white_list:
-                print('wifi point {} in white wifi lists'.format(wifi_point))
-                print('check connection')
-                connection = check_connection(wifi_point)
-                if connection:
-                    print('CONNECTED SUCCESSFULLY: ', wifi_point)
-                    send_message_debug('CONNECTED SUCCESSFULLY')
-                    wifi_connection = True
-                    lend_wifi_status.on()
-                else:
-                    wifi_connection = False
-                    print('CONNECTED NOT ESTABLISHED')
-                    lend_wifi_status.off()
+            if len([x for x in white_list if x in set(wifi_list)]) > 0:
+                if wifi_point in white_list:
+                    print('wifi point {} in white wifi lists'.format(wifi_point))
+                    print('check connection')
+                    connection = check_connection(wifi_point)
+                    if connection:
+                        print('CONNECTED SUCCESSFULLY: ', wifi_point)
+                        send_message_debug('CONNECTED SUCCESSFULLY')
+                        wifi_connection = True
+                        lend_wifi_status.on()
+                    else:
+                        wifi_connection = False
+                        print('CONNECTED NOT ESTABLISHED')
+                        lend_wifi_status.off()
             else:
                 wifi_connection = False
                 print('WIFI NOT FOUND')
