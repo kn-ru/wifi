a1f640bd51c88f591eea76fab7662f5697281021
knru <rusakov.msk@ya.ru>
2021-07-19 15:09:02 +0300

    add new logic

--- utils.py
+++ utils.py
@@ -3,23 +3,25 @@ import os
 import requests
 import re
 import os
+import socket
 from dotenv import load_dotenv
 load_dotenv()
 
 test_urls = os.getenv("TEST_LISTS_URL").split(',')
 white_list = os.getenv("WHITE_LISTS").split(',')
-check_url = os.getenv("CHECKS_URL")
 test_check_url = os.getenv("INTERNET_CHECK_URL")
 TOKEN_BOT = os.getenv("TOKEN_BOT")
 CHAT_ID = os.getenv("CHAT_ID")
 message = os.getenv("MESSAGE")
+host = os.getenv("HOST")
+port = os.getenv("PORT")
 
 def send_message_debug(message):
     url = "https://api.telegram.org/bot" + TOKEN_BOT + "/sendMessage?chat_id=@" + CHAT_ID + "&text=" + message
     requests.post(url)
 
 def url_ok(url):
-    r = requests.head(url)
+    r = requests.post(url)
     print('status - ', r.status_code)
 
     return r.status_code == 200
@@ -38,7 +40,7 @@ def check_connection(wifi_point):
     if wifi_point in test_urls:
         return url_ok(test_check_url)
     elif wifi_point in white_list:
-        return url_ok(check_url)
+        return url_ok(host + ':' + str(port))
     else:
         print('wrong connection')
         return False
@@ -48,4 +50,7 @@ def check_connection_mode(mode):
     else:
         return url_ok(check_url)
 def send_to_api(message):
-    print(message)
+    try:
+        requests.post(host + ':' + str(port), timeout=0.1)
+    except requests.exceptions.ReadTimeout:
+        pass
