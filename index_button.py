5ddd6cdd802a053bc8041739ddf55d99f31fec2f
Vladimir Semenovykh <mdscntst@MacBook-Pro-Vladimir.local>
2021-07-24 13:27:12 +0300

    add timeout 3 seconds

--- index_button.py
+++ index_button.py
@@ -10,14 +10,22 @@ button = Button(2)
 
 wifi_connection = False
 press_connection = False
+start = datetime.datetime.now()
 
 while True:
 
     try:
         button.wait_for_press()
+        delta = (datetime.datetime.now() - start).seconds
+        if delta > 3:
+            press_connection = False
         print('You pushed me')
         led_send.on()
-        send_to_api(message)
+        if not press_connection:
+            send_to_api(message)
+            press_connection = True
+            start = datetime.datetime.now()       
+        
         time.sleep(2)
         led_send.off()
 
